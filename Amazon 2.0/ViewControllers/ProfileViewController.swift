ab96d60e93f682ddcf049104ce382db97e442332
Joonaynay <Joonaynay@gmail.com>
2021-11-09 17:07:02 -0700

    Start

---
+++ Amazon 2.0/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Amazon 2.0
+//
+//  Created by Forrest Buhler on 11/9/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Amazon 2.0/ViewControllers/ShopViewController.swift
+++ Amazon 2.0/ViewControllers/ShopViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShopViewController: UIViewController {
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
     }
 
 
